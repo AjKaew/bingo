cbf30464088b43b9bc386af9b5a5817741b799b2
AjKaew <chaloemphon.sir@dpu.ac.th>
2023-10-29 01:48:15 +0700

    fix structure

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   // The location of the build folder described above
   output: {
     path: path.resolve(__dirname, 'js/dist'),
-    filename: 'js/dist/[name].js'
+    filename: '[name].js'
   },
   // Optional and for development only. This provides the ability to
   // map the built code back to the original source format when debugging.
